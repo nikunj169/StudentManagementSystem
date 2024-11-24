ac74e1d765b5209877476e7ca988ddb61b3196df
aloofdevil <ishaansodhi05@gmail.com>
2024-11-24 20:13:17 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,14 @@
+package sms;
+
+import java.util.Date;
+
+enum Gender {
+	Male, Female
+}
+
+/**
+ * The class that holds and manages the information about students
+ * 
+ * @author Artiom
+ *
+ */
