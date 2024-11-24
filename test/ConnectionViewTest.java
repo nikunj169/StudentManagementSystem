2585f36551692fd553ac5383c554d7b92ab9d8da
aloofdevil <ishaansodhi05@gmail.com>
2024-11-24 21:25:27 +0530

    Add test class for validating the GUI of ConnectionView

--- test/ConnectionViewTest.java
+++ test/ConnectionViewTest.java
@@ -4,3 +4,11 @@ import org.fest.swing.fixture.FrameFixture;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+/**
+ * The class that tests ConnectionView class' GUI
+ * 
+ * @author Artiom
+ *
+ */
+public class ConnectionViewTest {
