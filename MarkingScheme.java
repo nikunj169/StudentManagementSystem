6b0406b0d74a04203f5d512e64a392cdf0691ecc
SWAYAMK44 <158851508+SWAYAMK44@users.noreply.github.com>
2024-11-24 20:38:54 +0530

    Update MarkingScheme.java

--- MarkingScheme.java
+++ MarkingScheme.java
@@ -1,1 +1,7 @@
 package sms;
+ublic class MarkingScheme {
+    private int id;
+    private int courseId;
+    private double gradeA;
+    private double gradeAMinus;
+    private double gradeBPlus;
