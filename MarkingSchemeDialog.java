589d92dffcfc76c2d7fb3cac0c1f2c665384a649
SWAYAMK44 <158851508+SWAYAMK44@users.noreply.github.com>
2024-11-24 20:31:27 +0530

    Update MarkingSchemeDialog.java

--- MarkingSchemeDialog.java
+++ MarkingSchemeDialog.java
@@ -5,3 +5,11 @@ public class MarkingSchemeDialog extends JDialog {
             gradeDField, gradeFField;
     private JButton saveButton;
 }
+private void initialize() {
+        setSize(400, 400);
+        setLocationRelativeTo(null);
+        setLayout(new GridLayout(9, 2));
+
+        add(new JLabel("Grade A:"));
+        gradeAField = new JTextField();
+        add(gradeAField);
