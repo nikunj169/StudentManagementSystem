9dc4802c93ec5e70e45e1e6fb7f7244682dc6870
SWAYAMK44 <158851508+SWAYAMK44@users.noreply.github.com>
2024-11-24 20:30:17 +0530

    Create MarkingSchemeDialog.java

---
+++ MarkingSchemeDialog.java
@@ -0,0 +1,7 @@
+public class MarkingSchemeDialog extends JDialog {
+    private int courseId;
+    private MarkingScheme markingScheme;
+    private JTextField gradeAField, gradeAMinusField, gradeBPlusField, gradeBField, gradeBMinusField, gradeCField,
+            gradeDField, gradeFField;
+    private JButton saveButton;
+}
