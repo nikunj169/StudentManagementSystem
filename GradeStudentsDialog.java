b7e99961a99ee25bc204f01e8a9cc561dc6986c9
ShivekRanjan <152221558+ShivekRanjan@users.noreply.github.com>
2024-11-24 20:50:11 +0530

    Update GradeStudentsDialog.java

--- GradeStudentsDialog.java
+++ GradeStudentsDialog.java
@@ -5,3 +5,9 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.List;
 import java.util.Arrays;
+public class GradeStudentsDialog extends JDialog {
+    private JComboBox<String> facultyComboBox;
+    private JComboBox<String> courseComboBox;
+    private JButton nextButton;
+    private JPanel contentPanel;
+    private ManagementView parentView;
