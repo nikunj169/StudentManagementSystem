2f3cf2b9d202c7fafb900b07ea4fc5be2debf64e
aloofdevil <ishaansodhi05@gmail.com>
2024-11-26 21:47:55 +0530

    Create DBHandler class to manage database connection and table names.

--- DBHandler.java
+++ DBHandler.java
@@ -13,3 +13,48 @@ import java.util.HashMap;
 import java.util.Vector;
 
 import javax.swing.table.DefaultTableModel;
+
+/**
+ * The class that allows access to a database for reading and writing data
+ * purposes
+ * 
+ * @author Artiom
+ *
+ */
+public class DBHandler {
+	/**
+	 * Login to connect to the database
+	 */
+	private static String login;
+
+	/**
+	 * Password to connect to the database
+	 */
+	private static String password;
+
+	/**
+	 * Database URL
+	 */
+	static String databaseUrl;
+
+	/**
+	 * The var that stores students table's name
+	 */
+	private final static String studentsTable;
+
+	/**
+	 * The var that stores courses table's name
+	 */
+	private final static String coursesTable;
+
+	/**
+	 * The var that stores faculties table's name
+	 */
+	private final static String facultiesTable;
+
+	/**
+	 * Default constructor
+	 */
+	public DBHandler() {
+
+	}
