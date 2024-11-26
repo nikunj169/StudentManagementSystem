268ce343293fbe6d238f6966843fa4ea7d107fdd
aloofdevil <ishaansodhi05@gmail.com>
2024-11-26 21:46:38 +0530

    Create DBHandler.java

---
+++ DBHandler.java
@@ -0,0 +1,15 @@
+package sms;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Vector;
+
+import javax.swing.table.DefaultTableModel;
