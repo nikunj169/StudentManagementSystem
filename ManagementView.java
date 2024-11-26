2d02d34c650b62ff989ceceff7b201528f94ddde
Akshattyagi1807 <yashmee1807@gmail.com>
2024-11-26 22:40:45 +0530

    Add front-end UI for student management system with table display and actions

---
+++ ManagementView.java
@@ -0,0 +1,36 @@
+package sms;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.border.LineBorder;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
+
+/**
+ * The class that holds the front-end table-management part of the application
+ * and manages the actions performed out there
+ * 
+ * @author Artiom
+ *
+ */
